b7e9a274a07aa65423d3ee7f5a938fab9825deb0
l3m0n42 <Alberto.G.Yerena@gmail.com>
2022-10-27 15:50:54 -0700

    I am based now

--- snortDocker.sh
+++ snortDocker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -eu -o pipefail
-
 if [ "$EUID" -ne 0 ]; then
 	echo 'script requires root privileges'
 	exit 1
@@ -8,8 +6,14 @@ fi
 
 manager_detection(){	
 	if [ $(command -v apt) ]; then
+		apt remove docker docker-engine docker.io containerd runc
+		apt install ca-certificates curl gnupg lsb-release -y
+		mkdir -p /etc/apt/keyrings
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		chmod a+r /etc/apt/keyrings/docker.gpg
 		apt update
-		apt install docker.io -y
+		apt install docker.io docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 		systemctl start docker
 	elif [ $(command -v yum) ]; then
 		remove docker \
@@ -47,8 +51,8 @@ container_install(){
 		docker run -it --rm --net=host linton/docker-snort /bin/bash
 		snort -i $interface -c /etc/snort/etc/snort.conf -A console	
 	
-	else [[$(echo 'Installing Snort container <3')]]; then
-		docker pull linton/docker-snort
+	else [[$(echo 'Installing Snort container <3')]];
+		docker pull plinton/docker-snort
                 read -p 'desired interface for docker: ' interface
                 docker run -it --rm --net=host linton/docker-snort /bin/bash
                 snort -i $interface -c /etc/snort/etc/snort.conf -A console
