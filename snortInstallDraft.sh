f41bb1889101bb4f499bc0669245e9161adbea37
l3m0n42 <101772709+l3m0n42@users.noreply.github.com>
2022-10-26 21:25:41 -0700

    Updates

--- snortInstallDraft.sh
+++ snortInstallDraft.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eu -o pipefail
+
 if [ "$EUID" -ne 0 ]; then
 	echo 'script requires root privileges'
 	exit 1
@@ -11,9 +13,11 @@ fi
 manager_detection(){
 
 	if [ $(command -v apt) ]; then
-		apt install build-essential bison flex libpcap-dev libpcre3 libpcre3-dev libdumbnet-dev zlib1g-dev libnghttp2-dev openssl libdnet apache2 libapache2-mod-php libphp-adodb php-pear libwww-perl php-gd -y
-
+		apt install build-essential bison flex libpcap-dev libpcre3 libpcre3-dev libdumbnet-dev zlib1g-dev libnghttp2-dev openssl libdnet apache2 libapache2-mod-php libphp-adodb php-pear libwww-perl php-gd libntirpc-dev -y
+		cp /usr/include/ntirpc/rpc/* /usr/include/rpc
 		cd /tmp
+		mkdir ./snort-source-files
+		cd snort-source-files
 		wget https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz
 		wget https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz
 		tar xvzf daq-2.0.7.tar.gz
@@ -22,7 +26,7 @@ manager_detection(){
 		cd ..
 		tar xvzf snort-2.9.20.tar.gz
 		cd snort-2.9.20
-		./configure --enable-sourcefire && make && sudo make install
+		./configure --enable-sourcefire --disable-open-appid && make && sudo make install
 
 	elif [ $(command -v yum) ]; then
         	yum install https://www.snort.org/downloads/snort/
